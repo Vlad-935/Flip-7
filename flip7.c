2baedece770801d9460da224bc1245d4b766c869
Vlad-935 <maracinevlad25@gmail.com>
2026-03-10 18:26:00 +0200

    Add main file 'flip7.c'

---
+++ flip7.c
@@ -0,0 +1,50 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cards.h"
+#include "player.h"
+
+void round(int player_count, deck cards, Players *player)
+{
+	for (int i = 0; i < player_count; i++) {
+		if (player[i].in_game) {
+			int option;
+			printf("Player %d: Hit/Stay\n", i + 1);
+			scanf("%d", &option);
+
+			if (option == 2) {	// stay
+				player[i].in_game = false;
+			} else {  // hit
+			}
+		}
+	}
+}
+
+void game(int player_count, deck cards, Players player)
+{
+	bool winner = false;
+
+	while (!winner) {
+		int active_players = player_count;
+		while (!active_players) {
+			round(player_count, cards, &player);
+		}
+	}
+}
+
+int main(void)
+{
+	int player_count;
+	deck cards;
+	card_setup(&cards);
+
+	printf("Introduceti numarul de jucatori: ");
+	scanf("%d", &player_count);
+
+	Players player = player_setup(player_count);
+
+	game(player_count, cards, player);
+
+	return 0;
+}
