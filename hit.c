c9d0e197735ccd8da18c73e8bd0128ec278f59dc
Vlad-935 <maracinevlad25@gmail.com>
2026-03-10 20:32:05 +0200

    New hit files for dealing cards

---
+++ hit.c
@@ -0,0 +1,8 @@
+#include "hit.h"
+
+#include <stdio.h>
+
+void hit(int cards[], Players player)
+{
+	int card = rand() % 22;
+}
---
+++ hit.h
@@ -0,0 +1,8 @@
+#ifndef HIT
+#define HIT
+
+#include "player.h"
+
+void hit(int cards[], Players player);
+
+#endif
