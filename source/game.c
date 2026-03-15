46b88c079261832bb7e41e6f36b2a8ce4d65a8c7
Vlad-935 <maracinevlad25@gmail.com>
2026-03-15 17:45:30 +0200

    Added new struct for round state, so it would be easier to handle, active players, turns and total players

--- source/game.c
+++ source/game.c
@@ -8,8 +8,7 @@
 #define diff_cards 22
 #define text_time 2000
 
-void make_choice(int *players_turn, int *active_players,
-				 deck *cards, Players *player)
+void make_choice(round_state *round, deck *cards, Players *player)
 {
 	int option;
 
@@ -18,29 +17,29 @@ void make_choice(int *players_turn, int *active_players,
 		"Player %d:\n"
 		"Points: %d\n"
 		"Current cards:\n",
-		*players_turn, player[*players_turn].total_points);
-	show_player_cards(player[*players_turn]);
+		round->players_turn, player[round->players_turn].total_points);
+	show_player_cards(player[round->players_turn]);
 	printf("Hit/Stay\n");
 
 	scanf("%d", &option);
 
 	switch (option) {
 		case 1:	 // hit
-			hit(cards, &player[*players_turn]);
+			hit(cards, &player[round->players_turn]);
 
-			update_bust_state(&player[*players_turn]);	// Set player as busted if they have 2 dublicates
-			if (player[*players_turn].busted) {
+			update_bust_state(&player[round->players_turn]);  // Set player as busted if they have 2 dublicates
+			if (player[round->players_turn].busted) {
 				clear_screen();
-				printf("Player %d busted!\n", *players_turn);
+				printf("Player %d busted!\n", round->players_turn);
 				delay_ms(text_time);
 
-				(*active_players)--;
+				round->active_players--;
 			}
 
 			break;
 		case 2:	 // stay
-			player[*players_turn].in_game = false;
-			(*active_players)--;
+			player[round->players_turn].in_game = false;
+			round->active_players--;
 
 			break;
 		default:  // error
@@ -48,13 +47,17 @@ void make_choice(int *players_turn, int *active_players,
 			printf("Not an option, try again.\n");
 			delay_ms(text_time);
 
-			(*players_turn)--;
+			round->players_turn--;
 	}
 }
 
-void new_round_setup(int player_count, Players *player)
+void new_round_setup(round_state *round, Players *player)
 {
-	for (int i = 1; i <= player_count; i++) {
+	round->flip7 = false;
+	round->players_turn = 1;
+	round->active_players = round->player_count;
+
+	for (int i = 1; i <= round->player_count; i++) {
 		player[i].in_game = true;
 		player[i].busted = false;
 
@@ -64,41 +67,38 @@ void new_round_setup(int player_count, Players *player)
 	}
 }
 
-void game_round(int player_count, deck *cards, Players *player)
+void game_round(round_state round, deck *cards, Players *player)
 {
 	// New round
-	int players_turn = 1, active_players = player_count;
-	bool flip7 = false;
-	new_round_setup(player_count, player);
+	new_round_setup(&round, player);
 
 	// Play until everyone is out, or someone got a flip7
-	while (active_players && !flip7) {
-		if (players_turn > player_count) {
-			players_turn = 1;
+	while (round.active_players && !round.flip7) {
+		if (round.players_turn > round.player_count) {
+			round.players_turn = 1;
 		}
 
-		if (player[players_turn].in_game) {
-			make_choice(&players_turn, &active_players,
-						cards, player);
+		if (player[round.players_turn].in_game) {
+			make_choice(&round, cards, player);
 		}
 
-		if (player[players_turn].different_cards == 7) {
-			flip7 = true;
+		if (player[round.players_turn].different_cards == 7) {
+			round.flip7 = true;
 		}
 
-		players_turn++;
+		round.players_turn++;
 	}
 
-	end_round(player_count, player);
+	end_round(round.player_count, player);
 }
 
-void game(int player_count, deck *cards, Players *player)
+void game(round_state round, deck *cards, Players *player)
 {
 	bool winner = false;
 
 	// Play until one person reaches 200 points
 	while (!winner) {
-		game_round(player_count, cards, player);
+		game_round(round, cards, player);
 
 		clear_screen();
 		printf("Round ended!\n");
